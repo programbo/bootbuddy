0b55c4560eb78b00567ffe1759fa473c9665b7a3
John Lombardo <john@programbo.com>
2013-11-25 21:11:54 +0800

    Change the IP to 0.0.0.0 to access it externally from Nitrous.IO

--- protractor.conf.js
+++ protractor.conf.js
@@ -74,7 +74,7 @@ exports.config = {
   //
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
-    baseUrl: 'http://locahost:9000',
+    baseUrl: 'http://0.0.0.0:4000',
 
   // Selector for the element housing the angular app - this defaults to
   // body, but is necessary if ng-app is on a descendant of <body>
